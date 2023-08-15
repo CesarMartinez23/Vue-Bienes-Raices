d0459601d1aa7e674eacb920339d860c74bbe329
CesarMartinez23 <oliver-cesar@hotmail.com>
2023-08-14 19:01:00 -0900

    Navbar & Router

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,22 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
+  <v-card elevation="3" max-width="1200" class="mx-auto">
+    <v-layout>
+      <v-app-bar color="blue-darken-1">
+        <template v-slot:prepend>
+          <v-btn :to="{ name: 'home' }">INMOMAC - EL SALVADOR</v-btn>
+        </template>
+        <template v-slot:append>
+          <v-btn :to="{ name: 'home' }">Inicio</v-btn>
+          <v-btn :to="{ name: 'login' }">Iniciar Sesi&oacute;n</v-btn>
+        </template>
+      </v-app-bar>
+      <v-main>
+        <v-container>
+          <RouterView />
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
